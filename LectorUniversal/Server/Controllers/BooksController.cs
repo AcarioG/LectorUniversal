81190e89ee34ae796d16e69bb993e4717b643d66
wilmer vasquez de leon <wilmer@ceo.do>
2021-11-17 18:16:10 -0400

    create page added

--- LectorUniversal/Server/Controllers/BooksController.cs
+++ LectorUniversal/Server/Controllers/BooksController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using LectorUniversal.Server.Service;
+using LectorUniversal.Shared;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace LectorUniversal.Server.Controllers
@@ -7,5 +9,53 @@ namespace LectorUniversal.Server.Controllers
     [ApiController]
     public class BooksController : ControllerBase
     {
+        private readonly IBookServices _bookServices;
+
+        public BooksController(IBookServices bookServices)
+        {
+            _bookServices = bookServices;
+        }
+
+        //GET: api/Books
+        [HttpGet]
+        public async Task<IEnumerable<Book>> GetAsync()
+        {
+            var comics = await _bookServices.GetAllBooksAsync();
+            if (comics == null)
+            {
+                return (IEnumerable<Book>)NotFound();
+            }
+
+            return comics;
+        }
+
+
+        //POST: api/books
+        [HttpPost]
+        public async Task<ActionResult> PostAsync([FromBody] Book book)
+        {
+            await _bookServices.AddBooksAsync(book);
+            await _bookServices.SaveBooksAsync();
+
+            return NoContent();
+        }
+
+        //DELETE: api/Books/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            Book book = await _bookServices.GetBookAsync(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            await _bookServices.DeleteBooksAsync(book);
+            await _bookServices.SaveBooksAsync();
+
+            return NoContent();
+        }
+
     }
 }
