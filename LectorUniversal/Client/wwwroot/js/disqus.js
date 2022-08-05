e47a5b0ec477f1edc72ef1dc5014733d3a5aad9a
Wilmer Vásquez <wilmer@ceo.do>
2022-08-05 19:16:51 -0400

    fix bug en comentario

--- LectorUniversal/Client/wwwroot/js/disqus.js
+++ LectorUniversal/Client/wwwroot/js/disqus.js
@@ -1,14 +1,16 @@
-var disqus_config = function (url,identifier) {
-    // Replace PAGE_URL with your page's canonical URL variable
-    this.page.url = `${url}`;
+//var disqus_config = function (url,identifier) {
+//    // Replace PAGE_URL with your page's canonical URL variable
+//    this.page.url = `${url}`;
 
-    // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    this.page.identifier = `${identifier}`;
+//    // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+//    this.page.identifier = `${identifier}`;
+//};
+
+export function DisqusFunction(disqusThreadElement) { // DON'T EDIT BELOW THIS LINE
+     
+            var d = document, s = d.createElement('script');
+            s.src = 'https://lectoruniversal.disqus.com/embed.js';
+            s.async = true;
+            s.setAttribute('data-timestamp', +new Date());
+            disqusThreadElement.appendChild(s);
 };
-
-(function () { // DON'T EDIT BELOW THIS LINE
-    var d = document, s = d.createElement('script');
-    s.src = 'https://lectoruniversal.disqus.com/embed.js';
-    s.setAttribute('data-timestamp', +new Date());
-    (d.head || d.body).appendChild(s);
-})();
